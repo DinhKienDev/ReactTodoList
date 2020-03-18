61e86a59ac94fd1547e01e39927d8dbaeee6e41c
DinhKienDev <56972525+DinhKienDev@users.noreply.github.com>
2020-03-18 16:23:14 +0700

    Updates

---
+++ precache-manifest.f4344ae7702cfd656ded73778be5fe76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b7840428d430ad5b8ac7987d6e984af",
+    "url": "/ReactTodoList/index.html"
+  },
+  {
+    "revision": "97e41cc25e02df95c81b",
+    "url": "/ReactTodoList/static/css/main.c0ea2230.chunk.css"
+  },
+  {
+    "revision": "c1126bc1fb30b20d8cba",
+    "url": "/ReactTodoList/static/js/2.aafc5593.chunk.js"
+  },
+  {
+    "revision": "3b173ed738774b4bb64ac2b5ddb03f3b",
+    "url": "/ReactTodoList/static/js/2.aafc5593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e41cc25e02df95c81b",
+    "url": "/ReactTodoList/static/js/main.f7b6418d.chunk.js"
+  },
+  {
+    "revision": "ca996aa6a34dd786041d",
+    "url": "/ReactTodoList/static/js/runtime-main.94397bd5.js"
+  }
+]);
